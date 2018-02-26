6b1b74b96cea7f401444ba3e870c5e060d614216
Claudio Scafesi <scafesi@gmail.com>
2018-02-26 11:41:58 +0100

    second

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 var http = require("http");
-var server = http.createServer();
+var fs = require("fs");
 
-server.listen (3000, function(){
-console.log("listenig port 3000");
+function handler(request, response) {
+  console.log(request.url);
+  var endpoint = request.url;
+  if (endpoint === "/") {
+    fs.readFile(__dirname + "/public/index.html", function(error, file) {
+
+      if (error) {
+        console.error(error);
+        response.writeHead(404);
+      } else {
+        response.writeHead(200, {
+          "Content-type": "text/html"
+        });
+        response.write(file);
+      }
+      response.end();
+    });
+
+  } else if (endpoint === "/test") {
+    response.end("testing");
+
+  } else {
+    fs.readFile(__dirname + "/public/" + endpoint, function(error, file) {
+
+      if (error) {
+        console.error(error);
+        response.writeHead(404);
+      } else {
+        response.write(file);
+      }
+      response.end();
+    });
+    // response.writeHead(404);
+    // response.end();
+  }
+  // response.writeHead(200, {"Content-type": "text/html"});
+  // response.write("ciao ciao");
+  // response.end(CIAO);
+}
+
+var server = http.createServer(handler);
+
+server.listen(3001, function() {
+  console.log("listenig port 3001");
 });
